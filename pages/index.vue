4ee123482042b4bbd01f0e73fb5f34c630935b1c
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-03-15 12:42:52 -0300

    feat: new Index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,20 @@
 <template>
-  <div></div>
+  <div class="index-intro">
+    <div class="prose flow content">
+      <p>
+        Descubra como transformamos espaços em lugares inspiradores e
+        funcionais. A Luminares é uma empresa de arquitetura com mais de 10 anos
+        de experiência em projetos residenciais, comerciais e corporativos.
+        Explore nossos projetos e entre em contato para começarmos a criar o seu
+        próximo espaço ideal.
+      </p>
+    </div>
+    <PageFooter />
+  </div>
 </template>
+
+<style>
+.index-intro {
+  margin-top: 3rem;
+}
+</style>
