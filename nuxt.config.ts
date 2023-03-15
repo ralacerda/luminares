030b055b6e1fdf47c01cfae600025573f14336cc
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-03-15 15:40:04 -0300

    fix: missing html language attribute

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: { pageTransition: { name: "page", mode: "out-in" } },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+    head: { htmlAttrs: { lang: "pt-BR" } },
+  },
   modules: ["unplugin-icons/nuxt"],
 });
