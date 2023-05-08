fa11b23f0b8eb90e0c75b998aa6d0453fe3a062b
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-05-08 17:06:32 -0300

    feat: featured projects in project list

---
+++ components/ProjectDetails.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+defineProps<{
+  title: string;
+  imageUrl: string;
+}>();
+</script>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+};
+</script>
+
+<template>
+  <div
+    v-bind="$attrs"
+    class="project-header"
+    :style="{
+      '--background-image': `url(${imageUrl})`,
+    }"
+  >
+    <h3 class="content">{{ title }}</h3>
+  </div>
+  <div class="flow prose">
+    <slot />
+  </div>
+</template>
+
+<style>
+.project-header {
+  background-color: var(--black);
+  background-image: linear-gradient(to top, #000000bb 20%, transparent 60%),
+    var(--background-image);
+  background-position: 0 50%;
+  background-size: cover;
+  width: 100vw;
+  margin-left: 50%;
+  transform: translateX(-50%);
+  aspect-ratio: 2.5;
+  color: var(--white);
+  display: flex;
+  align-items: end;
+  padding-bottom: 1rem;
+  font-size: var(--size-5);
+  border: 1px solid var(--black);
+}
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -12,12 +12,10 @@
       <div class="index">
         <span>Featured Projects</span>
         <ul>
-          <li>Historic-Modern</li>
-          <li>Urban Retreat</li>
-          <li>Urban Dome</li>
-          <li>Glass House</li>
-          <li>Casa Serena</li>
-          <li>Live Space</li>
+          <li><a href="#historic-modern">Historic-Modern</a></li>
+          <li><a href="#urban-retreat">Urban Retreat</a></li>
+          <li><a href="#suntlit-tower">Suntlit Tower</a></li>
+          <li><a href="#casa-serena">Casa Serena</a></li>
         </ul>
       </div>
       <p>
@@ -40,6 +38,154 @@
         each project meets their needs and personal tastes.
       </p>
 
+      <ProjectDetails
+        title="Historic-Modern"
+        imageUrl="images/historico-moderno.jpg"
+        id="historic-modern"
+      >
+        <p>
+          <strong>Crafting the </strong>Historic-Modern project was a unique
+          challenge for Luminares Arq, as it required us to blend modern
+          elements with the historic charm of the original structure. The
+          project involved the renovation and modernization of a historic
+          building while preserving its original features and character.
+        </p>
+        <p>
+          The renovation of the historic building presented several challenges,
+          including ensuring compliance with building codes and regulations
+          while preserving the original structure's character and integrity.
+          Additionally, we had to work within the constraints of the existing
+          building layout while accommodating the client's modern design
+          preferences and needs.
+        </p>
+        <p>
+          Despite these challenges, Luminares Arq's team was able to deliver an
+          exceptional outcome. We spent extensive time researching and sourcing
+          materials and finishes that would blend modern and historic elements
+          seamlessly. Our team also collaborated closely with the client to
+          ensure that their unique vision was incorporated into the final
+          design.
+        </p>
+        <p>
+          The entire project took approximately 18 months to complete. Through
+          our expert design and project management, we were able to create a
+          stunning, functional, and modern space while preserving the historic
+          building's character and charm.
+        </p>
+      </ProjectDetails>
+      <ProjectDetails
+        title="Urban Retreat"
+        imageUrl="images/urban-retreat.jpg"
+        id="urban-retreat"
+      >
+        <p>
+          <strong>For Luminares Arq</strong>, the Urban Retreat project was a
+          unique and exciting opportunity to transform an ordinary urban space
+          into a peaceful oasis in the heart of the city. The project involved
+          the renovation of a neglected building and the design of a tranquil
+          outdoor space that would provide a relaxing retreat for residents and
+          visitors alike.
+        </p>
+        <p>
+          One of the main challenges of the project was to create a sense of
+          privacy and seclusion within the busy urban environment. To achieve
+          this, the team at Luminares Arq incorporated a variety of elements,
+          including lush greenery, water features, and privacy screens. These
+          elements not only provided a sense of seclusion but also added to the
+          overall aesthetic appeal of the space.
+        </p>
+        <p>
+          The design of the outdoor space was inspired by natural landscapes and
+          traditional Japanese gardens, with a focus on creating a calming and
+          tranquil environment. To achieve this, the team carefully selected
+          plants and materials that would create a harmonious and serene
+          atmosphere. The end result was a beautifully landscaped outdoor space
+          that provided a peaceful oasis in the midst of a bustling urban
+          environment.
+        </p>
+        <p>
+          Overall, the Urban Retreat project was a success, providing residents
+          and visitors with a much-needed escape from the hustle and bustle of
+          city life. The project not only transformed a neglected building into
+          a beautiful outdoor space but also demonstrated Luminares Arq's
+          expertise in creating unique and inspiring designs that meet the needs
+          of their clients.
+        </p>
+      </ProjectDetails>
+      <ProjectDetails
+        title="Sunlit Tower"
+        imageUrl="images/sunlit-tower.jpg"
+        id="suntlit-tower"
+      >
+        <p>
+          <strong>The Sunlit Tower project </strong>was a remarkable undertaking
+          by Luminares Arq, which aimed to create a modern management office for
+          a university campus while incorporating green spaces and natural light
+          into the design. The building is characterized by its large structure
+          and its abundant windows, which allow for an abundance of natural
+          light to flow into the space.
+        </p>
+        <p>
+          The challenge was to create a workspace that would be both functional
+          and visually striking, while also being in harmony with the
+          surrounding greenery of the campus. Luminares Arq approached the
+          project with a focus on sustainability and eco-friendliness, utilizing
+          green roofs and incorporating natural materials such as wood and stone
+          into the design.
+        </p>
+
+        <p>
+          The result is a unique and inviting space that seamlessly integrates
+          the interior and exterior of the building, creating a sense of
+          openness and connection to nature. The abundance of natural light,
+          combined with the use of natural materials and greenery, creates a
+          calming and peaceful atmosphere that promotes productivity and
+          well-being among employees.
+        </p>
+
+        <p>
+          Overall, the Sunlit Tower project represents the perfect balance of
+          modern functionality and environmental consciousness, and stands as a
+          testament to Luminares Arq's commitment to creating innovative and
+          sustainable design solutions.
+        </p>
+      </ProjectDetails>
+      <ProjectDetails
+        title="Casa Serena"
+        imageUrl="images/casa-serena.jpg"
+        id="casa-serena"
+      >
+        <p>
+          <strong>Casa Serena is a </strong>residential project that is
+          currently in progress. The name "Casa Serena" comes from Spanish and
+          means "serene house". The idea behind this project is to create a
+          peaceful and calming space for the residents.
+        </p>
+        <p>
+          The design of the house reflects this concept by using simple and
+          clean lines, muted colors, and natural materials. The architects have
+          taken great care to create a space that feels open and welcoming, with
+          large windows and skylights that let in plenty of natural light. The
+          use of natural materials, such as wood and stone, helps to bring the
+          outside in, adding to the calming and serene atmosphere of the house.
+        </p>
+        <p>
+          Construction of Casa Serena is well underway, with the foundation and
+          framing complete. The team is now focusing on the interior finishes
+          and landscaping. The project is expected to take several more months
+          to complete, as the team is taking a meticulous approach to ensure
+          that every detail is perfect.
+        </p>
+        <p>
+          Casa Serena is not just a house, but a true sanctuary for those who
+          live there. The designers have taken great care to create a space that
+          is not just beautiful, but also functional and sustainable. From the
+          use of eco-friendly materials to the design of the space itself, every
+          aspect of Casa Serena is intended to create a sense of calm and
+          serenity. The result will be a truly special home that will be enjoyed
+          for years to come.
+        </p>
+      </ProjectDetails>
       <LinkHome />
     </div>
     <PageFooter />
